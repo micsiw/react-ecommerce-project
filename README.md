d2027e6ddd6bd36415f455ceb3860b5a88b5bcbd
micsiw <michalsiwanowicz@gmail.com>
2023-04-13 20:21:46 +0200

    fixed blog preview section, swapped swiper library for react slick

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Photo by Alyona Grishina on Unsplash
 Photo by Edz Norton on Unsplash
 Photo by Austin Wilcox on Unsplash
 Photo by Vince Mariel Conlu on Unsplash
-Photo by Annie Spratt on Unsplash
+Photo by Brian Lawson on Unsplash
 
 https://makeup-api.herokuapp.com/
--- src/components/Blog-preview.js
+++ src/components/Blog-preview.js
@@ -1,78 +1,122 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
-
-import "swiper/css";
-import "swiper/css/pagination";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import "../styles/Blog-preview.css";
-import articleImage1 from "./images/article-photo-1.jpg";
-import articleImage2 from "./images/article-photo-2.jpg";
-import articleImage3 from "./images/article-photo-3.jpg";
+import articleImage1 from "./images/article-photo-mobile-1.jpg";
+import articleImage2 from "./images/article-photo-mobile-2.jpg";
+import articleImage3 from "./images/article-photo-mobile-3.jpg";
 
 function BlogPreview() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 6000,
+          pauseonHover: true,
+          infinite: true,
+        },
+      },
+    ],
+  };
   return (
     <div className="blog-preview-section">
       <h3 className="blog-preview-title">Learn more in our blog</h3>
-      <div className="article-preview-scroller">
-        <Swiper pagination={true} modules={[Pagination]}>
-          <SwiperSlide>
+      <div className="slider-wrapper">
+        <Slider {...settings}>
+          <div className="slide-wrapper">
             <div className="blog-preview-article">
               <div className="preview-article-image">
                 <img src={articleImage1} alt="woman with makeup"></img>
               </div>
               <div className="preview-article-content">
                 <div className="preview-article-title">
-                  Acne Causes: What is acne and why do I have it?
+                  ACNE CAUSES: WHAT IS ACNE WAND WHY DO I HAVE IT?
                 </div>
                 <div className="preview-article-description">
                   The basics include a daily bath or shower and washing the
                   face.
                 </div>
-                <a href="" className="article-link">
-                  READ FULL ARTICLE
-                </a>
+                <div className="article-link-wrapper">
+                  <a href="" className="article-link">
+                    READ FULL ARTICLE
+                  </a>
+                </div>
               </div>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="blog-preview-article">
+          </div>
+          <div className="slide-wrapper">
+            <div className="blog-preview-article middle-article">
               <div className="preview-article-image">
                 <img src={articleImage2} alt="woman with makeup"></img>
               </div>
               <div class="preview-article-content">
                 <div className="preview-article-title">
-                  Acne Causes: What is acne and why do I have it?
+                  OUR ADVICE HOW TO DETERMINE YOUR SKIN TYPE.
                 </div>
                 <div className="preview-article-description">
-                  The basics include a daily bath or shower and washing the
-                  face.
+                  Short brief about the brands. Revolutionise your routine with
+                  luxury skin care you wont be able to live without.
                 </div>
-                <a href="" className="article-link">
-                  READ FULL ARTICLE
-                </a>
+                <div className="article-link-wrapper">
+                  <a href="" className="article-link">
+                    READ FULL ARTICLE
+                  </a>
+                </div>
               </div>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
+          </div>
+          <div className="slide-wrapper">
             <div className="blog-preview-article">
               <div className="preview-article-image">
                 <img src={articleImage3} alt="woman with makeup"></img>
               </div>
               <div class="preview-article-content">
                 <div className="preview-article-title">
-                  Acne Causes: What is acne and why do I have it?
+                  WHY MORNING ROUTINE IS SO IMPORTANT?
                 </div>
                 <div className="preview-article-description">
-                  The basics include a daily bath or shower and washing the
-                  face.
+                  For many of us, morning are a busy time. Getting ready,
+                  grabbing a bite to eat, and getting out...
                 </div>
-                <a href="" className="article-link">
-                  READ FULL ARTICLE
-                </a>
+                <div className="article-link-wrapper">
+                  <a href="" className="article-link">
+                    READ FULL ARTICLE
+                  </a>
+                </div>
               </div>
             </div>
-          </SwiperSlide>
-        </Swiper>
+          </div>
+        </Slider>
       </div>
     </div>
   );
